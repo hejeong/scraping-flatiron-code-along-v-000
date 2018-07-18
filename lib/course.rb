212ee5ec826de0d45408a0f837be36ebbf862888
Jonathan Hong <jonhong777@gmail.com>
2018-07-18 14:56:31 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,12 @@
-class Course 
+class Course
+  #class variables
+  @@all = []
 
+  #attribute accessors
+  attr_accessors :title, :schedule, :description
+
+  # class methods
+  def self.all
+    @@all
+  end
 end
