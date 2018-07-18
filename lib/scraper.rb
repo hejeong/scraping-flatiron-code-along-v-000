748a1d26e0f65050a142d035fb27b1801be44aa4
Jonathan Hong <jonhong777@gmail.com>
2018-07-18 15:23:14 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,7 +10,7 @@ class Scraper
 
       doc.css(".post").each do |post|
         course = Course.new
-        course.title = post.css("h2").text
+        
         course.schedule = post.css(".date").text
         course.description = post.css("p").text
       end
@@ -18,7 +18,7 @@ class Scraper
     end
 
     def get_courses
-
+      self.get_page.css('.post')
     end
 
     def make_courses
